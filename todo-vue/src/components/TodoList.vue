c6f8b36cc7fcd765c2457f6dd1efcc50edf5b23f
Yeon-YongHeum <yeon931@naver.com>
2019-11-18 15:02:42 +0900

    Django | settings CORS

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="todo-list">
-      <h2>투두</h2>
+      <li></li>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'TodoList'
+    name: 'TodoList',
 }
 </script>
 
--- todo-vue/src/views/Home.vue
+++ todo-vue/src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <TodoList/>
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TodoList from '@/components/TodoList.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
@@ -25,6 +26,7 @@ export default {
     axios.get('http://127.0.0.1:8000/api/v1/todos/')
     .then(response => {
       console.log(response) // 만약 오류가 발생하게 되면 ESLint 설정을 안해줬기때문에 
+      this.todos = response.data
     })
   }
 }
